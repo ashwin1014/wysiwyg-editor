91f442008cb783ba5b564cfd6f35096db1ba4aaa
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2018-11-27 17:08:07 +0530

    selection on arrow keys now working

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
- let commonFieldsArray = [
+$(document).on('keydown',function(e){
+    console.log(this, e.target);       
+    if($("#tagContainer").is(":visible")) {
+        let key = e.charCode || e.keyCode;
+        if(key == 37 || key == 38 || key == 39 || key == 40 ) {
+            e.preventDefault();
+        } else {}
+    }  
+});
+
+let commonFieldsArray = [
     { Name: "UniqueEventId", Value: "UniqueIdPlaceholder", key:"common-uniqueeventid" },
     { Name: "ProjectNameLbl", Value: "ProjectNamePlaceholder", key: "common-projectnamelbl" },
     { Name: "Status", Value: "FormCurrentStatusPlaceholder", key: "common-status"},
@@ -33,7 +43,7 @@ let executeEditorCommand = function(ele) {
     document.execCommand(command);
     doc.focus();
     doc.click();
-}
+};
 
 let debounce = function (func, wait, immediate) {
     var timeout;
@@ -54,7 +64,6 @@ let onContentKeypress = debounce(function(e){
  //  console.log(words)
  let selectionContext = document.getSelection().anchorNode.textContent;    
  var len = ul.getElementsByClassName('common-variables').length-1;
- 
 
     let tempSearch = "";
     for(let i=document.getSelection().focusOffset; i>=0; i--) {
@@ -65,50 +74,59 @@ let onContentKeypress = debounce(function(e){
         }
     }
 
+    // let checkString = $('#emailcontent').
+
     let checkLast = selectionContext.substr(document.getSelection().anchorOffset-2, 2);
-    console.log(checkLast)
+    console.log(checkLast);
 
     if(e.key === '@' && checkLast !== "@@" && checkLast.trim().length<=1){
         let coords = getSelectionCoords();
         tempSearch = selectionContext.substring(lastIndexOfAmp+1, document.getSelection().anchorOffset);
-        $("#tagContainer").show()
+        $("#tagContainer").show('fast')
         .css({
             'top': coords.y + 20, 
             'left': coords.x - 10
         });
        generateSuggestionsList(lastIndexOfAmp, true); 
-       $('.common-variables:not(.display-none, .variableHeader)')[0].classList.add('selected');
-             
+        $('.common-variables').not('.display-none').not('.variableHeader')[0].classList.add('selected');  
+    //    $('#emailContent').focusout();
+    //    $('#tags').focus();
     }
 
 
-    else if ($("#tagContainer").is(":visible") && e.keyCode === 38) {       //on up arrow   
-        var selected = $(".selected");
-        $("#tagContainer .common-variables").removeClass("selected");
-        if (selected.prev().length == 0) {
-            selected.siblings().last().addClass("selected");
-        } else {
-            selected.prev().addClass("selected");
-        }
+    else if ($("#tagContainer").is(":visible") && e.keyCode === 38) { //on up arrow   
+       
 
+        let selected = $(".selected").not( ".display-none" );
+        $("#tagContainer .element").removeClass("selected").not('.display-none');
+        if (selected.prev().length == 0) {
+            selected.siblings().last().addClass("selected").not('.display-none');
+        } else {
+            selected.prev().addClass("selected").not('.display-none');
+        }
+        e.preventDefault();
+     //   $('.element:not(:first-child).element-hover').removeClass('element-hover').prev().addClass('element-hover');
     }
 
     else if($("#tagContainer").is(":visible") && e.keyCode === 40){ // on down arrow
-        var selected = $(".selected");
-        $("#tagContainer .common-variables").removeClass("selected");
+
+        let selected = $(".selected").not( ".display-none" );
+        $("#tagContainer .element").removeClass("selected").not('.display-none');
         if (selected.next().length == 0) {
-            selected.siblings().first().addClass("selected");
+            selected.siblings().first().addClass("selected").not('.display-none');
         } else {
-            selected.next().addClass("selected");
+            selected.next().addClass("selected").not('.display-none');
         }
+        e.preventDefault();
+      //  $('.element:not(:last-child).element-hover').removeClass('element-hover').next().addClass('element-hover');
     }
 
 
-        else if ($("#tagContainer").is(":visible") && e.keyCode === 13 ){ 
-            e.preventDefault();   //on enter key        
-            $('.selected').click();
-        //    placeCaretAtEnd(document.getElementById('emailContent'));       
-        }
+    else if ($("#tagContainer").is(":visible") && e.keyCode === 13 ){ 
+        e.preventDefault();   //on enter key        
+        $('.selected').click();
+    //  placeCaretAtEnd(document.getElementById('emailContent'));       
+    }
 
 
     else if (/\s/g.test(selectionContext.substr(document.getSelection().anchorOffset-1, 1)) || selectionContext.substr(document.getSelection().anchorOffset-1, 1) === "") {//if space && @@
@@ -128,21 +146,43 @@ let onContentKeypress = debounce(function(e){
         tempSearch = selectionContext.substring(lastIndexOfAmp, document.getSelection().anchorOffset);
         filterItems(tempSearch);      
     }
+
+   
+    if($("#tagContainer").is(":visible")) {
+            setTimeout(function(){
+            $("#tagContainer").scrollTop(0);//set to top
+            let val = $('.selected').offset().top+50 - $("#tags").height();
+            $("#tagContainer").scrollTop(val);
+     },160);
+    }
+    
 },150);
 
 
-let subjectKeypress = function (event) {
-    if (event.keyCode === 13 && $("#tagContainer").is(":visible")) {
-        event.preventDefault();
+let contentKeypress = function (e) {
+    e = e || event;                
+    if (e.keyCode === 13 && $("#tagContainer").is(":visible")) {
+        e.preventDefault();
+        e.stopPropagation();       
     }
+
+};
+
+let onContentKeyup = function(e){
+    // e = e || event;                
+    // if (e.keyCode === 38 || e.keyCode === 40 && $("#tagContainer").is(":visible")) {
+    //     e.preventDefault();
+    //     e.stopPropagation();     
+    //     e.cancelbubble = true;
+    // }     
 };
 
 let generateSuggestionsList = function (ampPosition, isContent){
     $("div#tags").empty();
     let li = '';
-    li +='<button type="button" class="list-group-item variableHeader">Common</button>';
+    li +='<button type="button" class="list-group-item variableHeader element element-hover">Common</button>';
         commonFieldsArray.map(function(value, index){
-            li +='<button type="button" id="'+value.key+'" class="list-group-item white-border common-variables" onmouseenter="hoverIn(this)" onmouseleave="hoverOut(this)"  onclick="selectedTag(this,\'' + ampPosition + '\',\'' + isContent + '\')">'+value.Name+'</button>';
+            li +='<button type="button" id="'+value.key+'" class="list-group-item white-border common-variables element" onmouseenter="hoverIn(this)" onmouseleave="hoverOut(this)"  onclick="selectedTag(this,\'' + ampPosition + '\',\'' + isContent + '\')">'+value.Name+'</button>';
             $("#tags").append(li);
             li='';
             return li;
@@ -150,43 +190,43 @@ let generateSuggestionsList = function (ampPosition, isContent){
     };
 
     let hoverIn = function(ele) {
-        $("#tagContainer .common-variables").removeClass("selected");
-        ele.classList.add('selected')
-    }
+        $("#tagContainer .element").removeClass("selected");
+        ele.classList.add('selected');
+    };
 
     let hoverOut = function(ele) {
-        ele.classList.remove('selected')
-    }
+        ele.classList.remove('selected');
+    };
   
   
     let selectedTag = function(element, ampPosition, isContent){
         
         let textContent;
         if(element.textContent != undefined){
-            textContent = element.textContent
+            textContent = element.textContent;
         } else{
-            textContent =element.text()
+            textContent = element.text();
         }
         let valueToInsert = "[" + textContent + "]";
 
-        window.getSelection().anchorNode.textContent = window.getSelection().anchorNode.textContent.substr(0, (ampPosition - 0)-1)
-        + valueToInsert + window.getSelection().anchorNode.textContent.substr(window.getSelection().focusOffset, window.getSelection().anchorNode.textContent.length);
+    
+    window.getSelection().anchorNode.textContent = window.getSelection().anchorNode.textContent.substr(0, (ampPosition - 0) - 1) + valueToInsert + window.getSelection().anchorNode.textContent.substr(window.getSelection().focusOffset, window.getSelection().anchorNode.textContent.length);
 
-        tagContainer.style.display = "none";    
+    tagContainer.style.display = "none";    
+    $('#emailContent').blur();
 
-    }
+    };
 
     let filterItems = function (searchedWord) {
         let listItems = $('button.list-group-item.common-variables');
         let isContainerShowHide = false;
         for (let i = 0; i < listItems.length; i++) {
-            let listText = $('button.list-group-item.common-variables')[i].innerHTML;
-            if (listText.toLowerCase().match(searchedWord.toLowerCase())) {          
-                // $('button.list-group-item.common-variables')[i].style.display = '';
-                $('button.list-group-item.common-variables')[i].classList.remove('display-none');
+            let listText = listItems[i].innerHTML;
+            if (listText.toLowerCase().match(searchedWord.toLowerCase())) {       
+                listItems[i].classList.remove('display-none');
                 isContainerShowHide = true;
             } else {
-                $('button.list-group-item.common-variables')[i].classList.add('display-none');
+                listItems[i].classList.add('display-none');
             }
         }
         if (isContainerShowHide) $("#tagContainer").show();        
@@ -238,24 +278,6 @@ let getSelectionCoords = function() {
     return { x: x, y: y };
 };
 
-
-let placeCaretAtEnd = function (el) {
-    el.focus();
-    if (typeof window.getSelection != "undefined"
-        && typeof document.createRange != "undefined") {
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        range.collapse(false);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-    } else if (typeof document.body.createTextRange != "undefined") {
-        var textRange = document.body.createTextRange();
-        textRange.moveToElementText(el);
-        textRange.collapse(false);
-        textRange.select();
-    }
-};
 
 
 
